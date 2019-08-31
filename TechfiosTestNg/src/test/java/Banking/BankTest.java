79d9c03b0f0fc1b39314fef1c625ea8991c14594
anudahal <anudahal@192.168.0.27>
2019-08-30 21:35:40 -0600

    committing bank test

---
+++ TechfiosTestNg/src/test/java/Banking/BankTest.java
@@ -0,0 +1,18 @@
+package Banking;
+
+import org.testng.annotations.Test;
+
+public class BankTest {
+
+	
+	@Test
+	
+	public void AddAccount() {
+
+		
+		System.out.println("This is  a test case");
+	
+	
+	}	
+	
+}
